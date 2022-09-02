dd7ba0a64411e3499ca068395b5fd37c453d55b0
Shah Jahan <shahjahansarfraz3973@gmail.com>
2022-09-02 16:06:18 +0500

    Completed

--- JS/todo.js
+++ JS/todo.js
@@ -1,25 +1,29 @@
-// let newTodo = [];
+let todos = ["Get Up Early", "Do The Chores"];
 
-// let input = prompt("Enter command");
+let input = prompt("Enter command");
 
-// input = input.toLowerCase();
+while (input !== "quit" && input !== "q") {
+  if (input === "new") {
+    const newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+    console.log(`${newTodo} has been added to the list.`);
+  } else if (input === "list") {
+    console.log("*********************");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i + 1}: ${todos[i]}`);
+    }
+    console.log("*********************");
+  } else if (input === "delete") {
+    const indexStr = prompt("Enter Index to delete");
+    const index = parseInt(indexStr);
+    if (!Number.isNaN(index)) {
+      const deleted = todos.splice(index - 1, 1);
+      console.log(`Ok, deleted ${deleted[0]}`);
+    } else {
+      console.log("Invalid index");
+    }
+  }
 
-// while(input !== "quit"){
-//     switch (input) {
-//         case "new":
-//             newTodo = prompt("Enter new todo");
-//             break;
-
-//         case "list":
-//             for(let list of newTodo){
-//                 console.log(list);
-//             }
-//             break;
-
-//         default:
-//             console.log("error!!");
-//             break;
-//     }
-// }
-
-alert("check alert");
+  input = prompt("Enter command");
+}
+console.log("App Quited");
