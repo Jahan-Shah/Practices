365e39eab98bb3435492f950dc4441e3fa4bf83d
Shah Jahan <Shah Jahan@DESKTOP-3IPNBKK>
2023-03-03 20:14:43 +0500

    in progress

--- VueJs/routing-vuejs/src/main.js
+++ VueJs/routing-vuejs/src/main.js
@@ -10,16 +10,16 @@ import App from './App.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: '/', redirect: '/teams' },
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers }
+        { path: '/teams/:teamId', component: TeamMembers, props: true },
+        { path: '/:notFound(.*)', redirect: '/teams' }
     ],
     linkActiveClass: 'active'
 });
 
-
 const app = createApp(App);
 
 app.use(router);
-
 app.mount('#app');
