6ea53e48d5d3090075229081abd879598ef3b126
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-04-14 22:28:02 +0500

    vuex

--- VueJs/vuex/src/main.js
+++ VueJs/vuex/src/main.js
@@ -13,7 +13,31 @@ const store = createStore({
         increment(state) {
             state.counter += 1;
         },
+        increase(state, payload) {
+            state.counter += payload.value;
+        }
     },
+    actions: {
+        increment(context) {
+            setTimeout(() => {
+                context.commit('increment');
+            }, 2000);
+        },
+        increase(context, payload) {
+            context.commit('increase', payload);
+        }
+    },
+    getters: {
+        finalCounter(state) {
+            return state.counter * 2;
+        },
+        normalizedCounter(_state, getters) {
+            const finalCounter = getters.finalCounter;
+            if (finalCounter < 0) return 0;
+            if (finalCounter > 100) return 100;
+            return finalCounter;
+        }
+    }
 });
 
 const app = createApp(App);
