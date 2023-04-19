aad718150211e2da73a31e8de57a5ede712ff5a8
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-04-19 16:46:36 +0500

    vuex

--- VueJs/vuex/src/main.js
+++ VueJs/vuex/src/main.js
@@ -3,11 +3,11 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
+const counterModule = {
     state() {
         return {
             counter: 0,
-        };
+        }
     },
     mutations: {
         increment(state) {
@@ -15,7 +15,7 @@ const store = createStore({
         },
         increase(state, payload) {
             state.counter += payload.value;
-        }
+        },
     },
     actions: {
         increment(context) {
@@ -25,17 +25,50 @@ const store = createStore({
         },
         increase(context, payload) {
             context.commit('increase', payload);
-        }
+        },
     },
     getters: {
         finalCounter(state) {
-            return state.counter * 2;
+            return state.counter;
         },
         normalizedCounter(_state, getters) {
             const finalCounter = getters.finalCounter;
             if (finalCounter < 0) return 0;
             if (finalCounter > 100) return 100;
             return finalCounter;
+        },
+    },
+}
+
+const store = createStore({
+    modules: {
+        numbers: counterModule
+    },
+    state() {
+        return {
+            isLogIn: false,
+        };
+    },
+    mutations: {
+
+        setAuth(state, payload) {
+            state.isLogIn = payload.isAuth;
+        },
+    },
+    actions: {
+
+        login(context) {
+            context.commit('setAuth', { isAuth: true });
+        },
+        logout(context) {
+            context.commit('setAuth', { isAuth: false });
+        }
+
+    },
+    getters: {
+
+        userIsAuthenticated(state) {
+            return state.isLogIn;
         }
     }
 });
