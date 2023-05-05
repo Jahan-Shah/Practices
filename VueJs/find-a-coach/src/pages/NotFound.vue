8b841d123d0fcdbfe4d2596ec6e4cf9dde1fd789
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-05-05 21:43:12 +0500

    added not found page

--- VueJs/find-a-coach/src/pages/NotFound.vue
+++ VueJs/find-a-coach/src/pages/NotFound.vue
@@ -1,5 +1,15 @@
 <script></script>
 
-<template></template>
+<template>
+  <section>
+    <BaseCard>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all out
+        <routerLink to="/coaches">coaches</routerLink>.
+      </p>
+    </BaseCard>
+  </section>
+</template>
 
 <style scoped></style>
