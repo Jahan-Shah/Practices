4d374919d9e5301d3169951cf7a2ac94cff3432e
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-05-10 19:00:45 +0500

    added autologin

--- VueJs/find-a-coach/src/router.js
+++ VueJs/find-a-coach/src/router.js
@@ -36,7 +36,7 @@ const router = createRouter({
 
 router.beforeEach((to, _from, next) => {
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) next('/auth');
-  else if (to.meta.requiresAuth && store.getters.isAuthenticated) next('/');
+  else if (to.meta.requiresUnAuth && store.getters.isAuthenticated) next('/');
   else next();
 })
 
--- VueJs/find-a-coach/src/store/modules/auth/actions.js
+++ VueJs/find-a-coach/src/store/modules/auth/actions.js
@@ -1,38 +1,42 @@
-async function authenticate(url, payload) {
-  const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      email: payload.email,
-      password: payload.password,
-      returnSecureToken: true,
-    })
-  });
-
-  const responseData = await response.json();
-
-  if (!response.ok) {
-    console.log(responseData);
-    const error = new Error(responseData.message || "Failed to authenticate. Check your credentials");
-    throw error;
-  }
-
-  console.log(responseData);
-  return responseData;
-}
-
 export default {
   async login(context, payload) {
-    const responseData = await authenticate(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${import.meta.env.VITE_API_KEY}`, payload);
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
     })
   },
 
   async signup(context, payload) {
-    const responseData = await authenticate(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${import.meta.env.VITE_API_KEY}`, payload);
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    })
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${import.meta.env.VITE_API_KEY}`;
+
+    if (mode === 'signup') url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${import.meta.env.VITE_API_KEY}`;
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      })
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || "Failed to authenticate. Check your login details");
+      throw error;
+    }
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
 
     context.commit('setUser', {
       token: responseData.idToken,
@@ -40,6 +44,20 @@ export default {
       tokenExpiration: responseData.expiresIn
     })
   },
+
+  autoLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null
+      });
+    }
+  },
+
   logout(context) {
     context.commit('setUser', {
       token: null,
@@ -48,58 +66,3 @@ export default {
     });
   }
 };
-
-
-// export default {
-//   async login() {
-//     const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${import.meta.env.VITE_API_KEY}`, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         email: payload.email,
-//         password: payload.password,
-//         returnSecureToken: true,
-//       })
-//     });
-
-//     const responseData = await response.json();
-
-//     if (!response.ok) {
-//       console.log(responseData);
-//       const error = new Error(responseData.message || "Failed to authenticate. Check your login details");
-//       throw error;
-//     }
-
-//     console.log(responseData);
-//     context.commit('setUser', {
-//       token: responseData.idToken,
-//       userId: responseData.localId,
-//       tokenExpiration: responseData.expiresIn
-//     })
-//   },
-
-//   async signup(context, payload) {
-//     const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${import.meta.env.VITE_API_KEY}`, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         email: payload.email,
-//         password: payload.password,
-//         returnSecureToken: true,
-//       })
-//     });
-
-//     const responseData = await response.json();
-
-//     if (!response.ok) {
-//       console.log(responseData);
-//       const error = new Error(responseData.message || "Failed to authenticate. Check your login details");
-//       throw error;
-//     }
-
-//     console.log(responseData);
-//     context.commit('setUser', {
-//       token: responseData.idToken,
-//       userId: responseData.localId,
-//       tokenExpiration: responseData.expiresIn
-//     })
-//   }
-// };
